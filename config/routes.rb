5f27750b4ffce5e70aebde8664b306a0c5268dc5
Lee Sing Jie <singjie@singjie.com>
2015-09-27 21:35:50 +0800

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'request' => 'request#index'
+  post 'request' => 'request#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
