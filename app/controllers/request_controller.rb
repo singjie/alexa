b254d9df06c5b701c3726f68a5003fd0db1b4087
Lee Sing Jie <singjie@singjie.com>
2015-09-27 21:46:30 +0800

    Update

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,7 +1,7 @@
 class RequestController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
-    Rails.logger "YOU REQUESTED FOR ME?"
+    #Rails.logger "YOU REQUESTED FOR ME?"
     Rails.logger request.body.read
     #puts request.env
   end
