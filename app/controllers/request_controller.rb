d22afe7ada50ab87f7b941eb04756518b31abebb
Lee Sing Jie <singjie@singjie.com>
2015-09-27 22:13:29 +0800

    Update

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,8 +1,27 @@
 class RequestController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
-    puts "YOU REQUESTED FOR ME?"
-    puts request.body.read
+    #puts "YOU REQUESTED FOR ME?"
+    #puts request.body.read
+    #puts params["request"]["intent"]
+    name = params["request"]["intent"]["name"]
+    puts "Name #{name}"
+
+    if name == "GetLuckyNumbers"
+      return haze_intent
+    end
+
+    slots = params["request"]["intent"]["slots"]
+
+    slots.each do |key, value|
+      puts "key:#{key}"
+      puts "value:#{value}"
+    end
+    #puts params["session"]
     #puts request.env
+  end
+
+  private def haze_intent
+    
   end
 end
--- app/views/request/index.json.jbuilder
+++ app/views/request/index.json.jbuilder
@@ -1,1 +1,8 @@
-json.test "hello"
+json.version "1.0"
+json.response do
+  json.outputSpeech do
+    json.type "PlainText"
+    json.text "Hello la"
+  end
+end
+json.shouldEndSession true
